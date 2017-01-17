4ebbc0cd969ea719bbb2c38c2d2197db401e7268
aditya-jois <aditya-jois@users.noreply.github.com>
2017-01-17 18:57:20 +0530

    Create Television.java

---
+++ Television.java
@@ -0,0 +1,44 @@
+public class Television implements ITelevision {
+
+	@Override
+	public void powerOn() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void powerOff() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int nextChannel(int currentChannel) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int previousChannel(int currentChannel) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int increaseVolume(int currentVolume) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int decreaseVolume(int currentVolume) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
