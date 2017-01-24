914d93a18050e3339212a88fb5545c5744ff3e51
aditya-jois <aditya-jois@users.noreply.github.com>
2017-01-24 14:33:26 +0530

    Create Operators.java

---
+++ Operators.java
@@ -0,0 +1,67 @@
+public class Operators {
+	
+	public static void main(String[] args) {
+		/*
+		The following quick reference summarizes the operators supported by the Java programming language.
+		
+		Simple Assignment Operator
+		=       Simple assignment operator
+		
+		Arithmetic Operators
+		+       Additive operator (also used
+		        for String concatenation)
+		-       Subtraction operator
+		*       Multiplication operator
+		/       Division operator
+		%       Remainder operator
+		
+		Unary Operators
+		+       Unary plus operator; indicates
+		        positive value (numbers are 
+		        positive without this, however)
+		-       Unary minus operator; negates
+		        an expression
+		++      Increment operator; increments
+		        a value by 1
+		--      Decrement operator; decrements
+		        a value by 1
+		!       Logical complement operator;
+		        inverts the value of a boolean
+		        
+		The increment/decrement operators can be applied before (prefix) or after (postfix) the operand.
+		The code result++; and ++result; will both end in result being incremented by one.
+		The only difference is that the prefix version (++result) evaluates to the incremented value, whereas the postfix version (result++) evaluates to the original value.
+		If you are just performing a simple increment/decrement, it doesn't really matter which version you choose.
+		But if you use this operator in part of a larger expression, the one that you choose may make a significant difference.
+
+		Equality and Relational Operators
+		==      Equal to
+		!=      Not equal to
+		>       Greater than
+		>=      Greater than or equal to
+		<       Less than
+		<=      Less than or equal to
+		
+		Conditional Operators
+		&&      Conditional-AND
+		||      Conditional-OR
+		?:      Ternary (shorthand for 
+		        if-then-else statement)
+
+		Type Comparison Operator
+		instanceof      Compares an object to 
+		                a specified type
+
+		Bitwise and Bit Shift Operators
+		~       Unary bitwise complement
+		<<      Signed left shift
+		>>      Signed right shift
+		>>>     Unsigned right shift
+		&       Bitwise AND
+		^       Bitwise exclusive OR
+		|       Bitwise inclusive OR
+
+		 */
+	}
+
+}
